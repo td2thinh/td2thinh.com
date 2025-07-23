2d5d1f51a783f4899c1b72703b6f2980e5fa4157
td2thinh <75013885+td2thinh@users.noreply.github.com>
2025-07-23 22:54:21 +0200

    chore: change fonts

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -36,14 +36,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Source Code Pro",
+      font: "Source+Code+Pro",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Source Serif 4",
+      font: "Source+Serif+4",
       weight: 700,
       style: "bold",
     },
